8fe2d6cdab6d0cf11e4c58d96d73bbb5d564bae3
unknown <izazahmed0177@gmail.com>
2022-10-11 00:03:08 +0600

    footer complite

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -11,23 +11,9 @@ const Footer = () => {
               title="Company"
               className="inline-flex items-center"
             >
-              <svg
-                className="w-8 text-deep-purple-accent-400"
-                viewBox="0 0 24 24"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                strokeLinecap="round"
-                strokeMiterlimit="10"
-                stroke="currentColor"
-                fill="none"
-              >
-                <rect x="3" y="1" width="7" height="12" />
-                <rect x="3" y="17" width="7" height="6" />
-                <rect x="14" y="1" width="7" height="6" />
-                <rect x="14" y="11" width="7" height="12" />
-              </svg>
+              <img className='h-7' src={'science.png'} alt="" />
               <span className="ml-2 text-xl font-bold tracking-wide text-gray-800 uppercase">
-                Company
+              Programming Quizzes
               </span>
             </a>
             <div className="mt-6 lg:max-w-sm">
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -4,7 +4,7 @@ import { NavLink } from 'react-router-dom';
 const Navbar = () => {
     const [isMenuOpen, setIsMenuOpen] = useState(false);
     return (
-        <div className="bg-gray-900">
+        <div className="bg-neutral-600">
       <div className="px-4 py-5 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8">
         <div className="relative flex grid items-center grid-cols-2 lg:grid-cols-3">
           <ul className="flex items-center hidden space-x-8 lg:flex">
@@ -39,30 +39,11 @@ const Navbar = () => {
               </NavLink>
             </li>
           </ul>
-          {/* <a
-            href="/"
-            aria-label="Company"
-            title="Company"
-            className="inline-flex items-center lg:mx-auto"
-          >
-            <svg
-              className="w-8 text-teal-accent-400"
-              viewBox="0 0 24 24"
-              strokeLinejoin="round"
-              strokeWidth="2"
-              strokeLinecap="round"
-              strokeMiterlimit="10"
-              stroke="currentColor"
-              fill="none"
-            >
-              <rect x="3" y="1" width="7" height="12" />
-              <rect x="3" y="17" width="7" height="6" />
-              <rect x="14" y="1" width="7" height="6" />
-              <rect x="14" y="11" width="7" height="12" />
-            </svg> */}
+         
            
-            <a>
-            
+            <a className="inline-flex items-center">
+            <img className='h-7' src={'quiz.png'} alt="" />
+
 
             <span className="ml-2 text-xl font-bold tracking-wide text-gray-100">
             Programming Quizzes
@@ -129,9 +110,11 @@ const Navbar = () => {
                         className="p-2 -mt-2 -mr-2 transition duration-200 rounded hover:bg-gray-200 focus:bg-gray-200 focus:outline-none focus:shadow-outline"
                         onClick={() => setIsMenuOpen(false)}
                       >
-                        <svg className="w-5 text-gray-600" viewBox="0 0 24 24">
+                        {/* <svg className="w-5 text-gray-600" viewBox="0 0 24 24"> */}
+                        <svg className="w-5 text-white" viewBox="0 0 24 24">
                           <path
-                            fill="currentColor"
+                            // fill="currentColor"
+                            fill="block"
                             d="M19.7,4.3c-0.4-0.4-1-0.4-1.4,0L12,10.6L5.7,4.3c-0.4-0.4-1-0.4-1.4,0s-0.4,1,0,1.4l6.3,6.3l-6.3,6.3 c-0.4,0.4-0.4,1,0,1.4C4.5,19.9,4.7,20,5,20s0.5-0.1,0.7-0.3l6.3-6.3l6.3,6.3c0.2,0.2,0.5,0.3,0.7,0.3s0.5-0.1,0.7-0.3 c0.4-0.4,0.4-1,0-1.4L13.4,12l6.3-6.3C20.1,5.3,20.1,4.7,19.7,4.3z"
                           />
                         </svg>
