51bb3d913a913232fdeec011a038ad91337b337b
unknown <izazahmed0177@gmail.com>
2022-10-10 23:29:10 +0600

    navbar complite

--- src/components/Root/Root.jsx
+++ src/components/Root/Root.jsx
@@ -1,9 +1,15 @@
 import React from 'react';
+import { Outlet } from 'react-router-dom';
+import Footer from '../Footer/Footer';
+
+import Navbar from '../Navbar/Navbar';
 
 const Root = () => {
     return (
         <div>
-            <h1>root</h1>
+            <Navbar></Navbar>
+            <Outlet></Outlet>
+            <Footer></Footer>
         </div>
     );
 };
--- src/utils/routes.js
+++ src/utils/routes.js
@@ -1,3 +1,10 @@
+import About from "../components/About/About";
+import Blog from "../components/Blog/Blog";
+import ErrorPage from "../components/ErrorPage/ErrorPage";
+import Home from "../components/Home/Home";
+import Statistics from "../components/Statistics/Statistics";
+import Topics from "../components/Topics/Topics";
+
 const { createBrowserRouter } = require("react-router-dom");
 const { default: Root } = require("../components/Root/Root");
 
@@ -7,6 +14,34 @@ const router = createBrowserRouter([
     {
         path: '/',
         element: <Root></Root>,
+        errorElement: <ErrorPage></ErrorPage>,
+        children: [
+            {
+                path: '/',
+                element: <Home></Home>
+            },
+            {
+                path: '/home',
+                element: <Home></Home>
+            },
+            {
+                path: '/topics',
+                element: <Topics></Topics>
+            },
+            {
+                path: '/statistics',
+                element: <Statistics></Statistics>
+            },
+            {
+                path: '/blogs',
+                element: <Blog></Blog>
+            },
+            {
+                path: '/about',
+                element: <About></About>
+            }
+        ]
+
 
     },
 ])
