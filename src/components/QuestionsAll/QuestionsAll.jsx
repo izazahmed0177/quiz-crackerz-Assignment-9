0725f24758247657bd1aba2213c2980802f55878
unknown <izazahmed0177@gmail.com>
2022-10-11 10:51:32 +0600

    quizs data add

---
+++ src/components/QuestionsAll/QuestionsAll.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const QuestionsAll = () => {
+    return (
+        <div>
+            <h1>quc</h1>
+        </div>
+    );
+};
+
+export default QuestionsAll;
--- src/components/QuizQuestions/QuizQuestions.jsx
+++ src/components/QuizQuestions/QuizQuestions.jsx
@@ -1,13 +1,22 @@
 import React from 'react';
 import { useLoaderData } from 'react-router-dom';
+import QuestionsAll from '../QuestionsAll/QuestionsAll';
 
 const QuizQuestions = () => {
-    const question=useLoaderData()
-    console.log(question)
-    console.log(question.data)
+    const questionAllData=useLoaderData()
+    const questionAll=questionAllData.data
+    const allQuizs=questionAll.questions
+    console.log(allQuizs)
+    // console.log(questionAll)
+    // console.log(questionAll.data)
+    // console.log(questionAll.data.questions)
     return (
         <div>
-            <h1>quuuuuuuuu</h1>
+            <h1 className='text-center text-5xl font-bold m-3'>Quiz of: {questionAll.name} </h1>
+            {
+                allQuizs.map(quizs=><QuestionsAll key={quizs.id} quizs={quizs}></QuestionsAll>)
+
+            }
         </div>
     );
 };
