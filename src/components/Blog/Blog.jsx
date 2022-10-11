9a50bac0b0fd22f14ba36551abe2af9999a9550b
unknown <izazahmed0177@gmail.com>
2022-10-11 14:22:18 +0600

    about page add

--- src/components/Blog/Blog.jsx
+++ src/components/Blog/Blog.jsx
@@ -2,9 +2,26 @@ import React from 'react';
 
 const Blog = () => {
     return (
-        <div>
-            <h1>Blog</h1>
+        <section className="dark:dark:bg-gray-800 dark:dark:text-gray-100">
+        <div className="container flex flex-col justify-center px-4 py-8 mx-auto md:p-8">
+            <h2 className="mb-12 text-4xl font-bold leading-none text-center sm:text-5xl">Frequently Asked Questions</h2>
+            <div className="divide-y divide-gray-700">
+                <div className="py-6 space-y-2 md:grid md:grid-cols-12 md:gap-8 md:space-y-0">
+                    <h3 className="font-semibold md:col-span-12 text-2xl">1.What is the purpose of react router-dom?</h3>
+                    <p className="md:pl-0 md:col-span-12">Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde neque in fugiat magni, quas animi enim veritatis deleniti ex. Impedit.</p>
+                </div>
+                <div className="py-6 space-y-2 md:grid md:grid-cols-12 md:gap-8 md:space-y-0">
+                    <h3 className="font-semibold md:col-span-12 text-2xl">2.How  does context api works in react ?</h3>
+                    <p className="md:pl-0 md:col-span-12">Lorem ipsum dolor sit amet consectetur adipisicing elit. Est aspernatur quae, eos explicabo odit minima libero veniam similique quibusdam doloribus facilis ipsa accusantium vel maiores corrupti! Libero voluptate a doloribus?</p>
+                </div>
+                <div className="py-6 space-y-2 md:grid md:grid-cols-12 md:gap-8 md:space-y-0">
+                    <h3 className="font-semibold md:col-span-12 text-2xl">3.How does useRef work in React ?</h3>
+                    <p className="md:pl-0 md:col-span-12 ">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut voluptates aspernatur dolores in consequatur doloremque inventore reprehenderit, consequuntur perspiciatis architecto.</p>
+                </div>
+               
+            </div>
         </div>
+    </section>
     );
 };
 
--- src/components/QuestionsAll/QuestionsAll.jsx
+++ src/components/QuestionsAll/QuestionsAll.jsx
@@ -1,9 +1,56 @@
 import React from 'react';
 
-const QuestionsAll = () => {
+const QuestionsAll = ({quizs}) => {
+    const {id,question,options}=quizs
+    console.log(options)
     return (
         <div>
-            <h1>quc</h1>
+            <h1>question :{question}</h1>
+            <section className='text-gray-600 bg-slate-200'>
+                <div className='container px-5 py-24 mx-auto'>
+                    <div className='flex flex-col text-center w-full mb-20'>
+                    <h1 class="sm:text-4xl text-3xl font-medium title-font mb-2 text-gray-900">Question :{question}</h1>
+
+                    </div>
+
+                    <div className='lg:w-2/3 w-full mx-auto overflow-auto'>
+                    {/* <div className='border-t-2 border-b-2 border-gray-500 px-4 py-3 flex flex-row gap-3'> */}
+                    <div className=''>
+                    
+                   
+
+                
+                   
+
+                    
+
+
+                           <div class="flex items-center pl-4 rounded border border-gray-200 dark:border-gray-700">
+                                  <input id="bordered-radio-1" type="radio" value="" name="bordered-radio" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600" />
+                                  <label for="bordered-radio-1" class="py-4 ml-2 w-full text-sm font-medium text-gray-900 dark:text-gray-300">{options[0]}</label>
+                            </div>
+                           <div class="flex items-center pl-4 rounded border border-gray-200 dark:border-gray-700">
+                                <input checked id="bordered-radio-2" type="radio" value="" name="bordered-radio" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600" />
+                                <label for="bordered-radio-2" class="py-4 ml-2 w-full text-sm font-medium text-gray-900 dark:text-gray-300">{options[1]}</label>
+                           </div>
+
+
+
+
+
+
+
+
+                        
+
+                        
+                     </div>
+
+                    </div>
+
+                </div>
+
+            </section>
         </div>
     );
 };
